fa122a0b3333ccfff1b0c8769951f4349181954a
Jay Patel <pateljay.7480@gmail.com>
2021-01-28 13:33:20 +0530

    View Blog

--- Blog_App/urls.py
+++ Blog_App/urls.py
@@ -3,4 +3,6 @@ from Blog_App import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('view/<int:id>/',views.blog_detail,name='view'),
+
 ]
--- Blog_App/views.py
+++ Blog_App/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from Blog_App.models import Post_Blog 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    all_blog = Post_Blog.objects.all()
+    return render(request,'index.html',{'all_blog':all_blog})
+
+def blog_detail(request,id):
+    detail = get_object_or_404(Post_Blog,pk=id)
+    return render(request,'view.html',{'detail':detail})
